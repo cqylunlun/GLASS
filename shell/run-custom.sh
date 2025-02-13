cf0907f87b0b10dad6aafac9b4ba68937885ae79
Qiyu Chen <cqylunlun@gmail.com>
2025-02-13 11:00:49 +0800

    add custom version

---
+++ shell/run-custom.sh
@@ -0,0 +1,37 @@
+datapath=/root/cqy/dataset/Custom
+augpath=/root/cqy/dataset/dtd/images
+classes=('class1' 'class2')
+flags=($(for class in "${classes[@]}"; do echo '-d '"${class}"; done))
+
+cd ..
+python main.py \
+    --gpu 0 \
+    --seed 0 \
+    --test ckpt \
+  net \
+    -b wideresnet50 \
+    -le layer2 \
+    -le layer3 \
+    --pretrain_embed_dimension 1536 \
+    --target_embed_dimension 1536 \
+    --patchsize 3 \
+    --meta_epochs 640 \
+    --eval_epochs 1 \
+    --dsc_layers 2 \
+    --dsc_hidden 1024 \
+    --pre_proj 1 \
+    --mining 1 \
+    --noise 0.015 \
+    --radius 0.75 \
+    --p 0.5 \
+    --step 20 \
+    --limit 392 \
+  dataset \
+    --distribution 2 \
+    --mean 0.5 \
+    --std 0.1 \
+    --fg 0 \
+    --rand_aug 1 \
+    --batch_size 8 \
+    --resize 288 \
+    --imagesize 288 "${flags[@]}" mvtec $datapath $augpath
